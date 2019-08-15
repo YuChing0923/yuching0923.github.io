dd0d91fd432a1d22c071c7c2ac775ef6daf3ccff
YuChing0923 <salia0923@gmail.com>
2019-08-15 18:48:53 +0800

    [mod][thef2e scroll game][js]

--- thef2e/week5_game/js/gamePlay.js
+++ thef2e/week5_game/js/gamePlay.js
@@ -7,7 +7,9 @@ const gamePlay = {
         this.load.image('bg_cloud_poor', 'images/bg_cloud_poor.png');
         this.load.image('bg_road', 'images/bg_road.png');
 
-        this.load.spritesheet('phone_operator', 'images/char_phone_operator.png', {frameWidth: 120, frameHeight: 120});
+        this.load.spritesheet('phone_operator', 'images/char_phone_operator.png', { frameWidth: 120, frameHeight: 120 });
+
+        this.gameStop = false; // 控制遊戲是否停止
     },
     create: function() {
         // 資源載入完成，加入遊戲物件及相關設定
@@ -15,7 +17,7 @@ const gamePlay = {
         this.bg_city = this.add.tileSprite(cw / 2, ch - 275, cw, 200, 'bg_city');
         this.bg_road = this.add.tileSprite(cw / 2, ch - 95, cw, 200, 'bg_road');
         this.bg_cloud_poor = this.add.tileSprite(cw / 2, ch - 240, cw, 150, 'bg_cloud_poor');
-        
+
         //設定人物位置
         this.player = this.add.sprite(150, 300, 'phone_operator');
         //設定動畫播放
@@ -30,9 +32,17 @@ const gamePlay = {
     },
     update: function() {
         // 遊戲狀態更新
+        if (this.gameStop) return;
+
         this.bg_sky_poor.tilePositionX += 2;
         this.bg_city.tilePositionX += 4;
         this.bg_road.tilePositionX += 3;
         // this.bg_cloud_poor.tilePositionX += 0;
+
+        // 啟動鍵盤事件
+        let cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.up.isDown) {
+            console.log('up');
+        }
     }
 }
