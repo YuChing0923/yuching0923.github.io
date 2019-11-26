4a8b538336dd532156a583249ce7ced85d0821d0
YuChing0923 <salia0923@gmail.com>
2019-11-27 00:09:39 +0800

    [mod][thef2e hotel][html]

---
+++ thef2e/week6_hotel/precache-manifest.88cd12a3075b971689c032d8f516a029.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5979e6eb8b5eeba0cace6805ba85337",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b927d904661f83a908f9",
+    "url": "./static/css/2.14169a37.chunk.css"
+  },
+  {
+    "revision": "ccf44b1abbb09d015921",
+    "url": "./static/css/main.3dddfb2a.chunk.css"
+  },
+  {
+    "revision": "b927d904661f83a908f9",
+    "url": "./static/js/2.a44dbfcc.chunk.js"
+  },
+  {
+    "revision": "ccf44b1abbb09d015921",
+    "url": "./static/js/main.a03afc4d.chunk.js"
+  },
+  {
+    "revision": "399c965f38c60b37f94c",
+    "url": "./static/js/runtime-main.ffe6dda8.js"
+  },
+  {
+    "revision": "475ad199d3923b1ce6404391e62ad246",
+    "url": "./static/media/envelope-solid.475ad199.svg"
+  },
+  {
+    "revision": "7df7570f7e5bb9b2f7dc29aded8c7238",
+    "url": "./static/media/facebook-square-brands.7df7570f.svg"
+  },
+  {
+    "revision": "1600889cd9dea93d957dd508ab0a4976",
+    "url": "./static/media/home-solid.1600889c.svg"
+  },
+  {
+    "revision": "929508038ccc98ec0099bb614307a762",
+    "url": "./static/media/info_icon-bar.92950803.svg"
+  },
+  {
+    "revision": "c4c2c9720464acf58e319946a14524bf",
+    "url": "./static/media/info_icon-breakfast.c4c2c972.svg"
+  },
+  {
+    "revision": "410d1c428a8a48ed5019e12476225018",
+    "url": "./static/media/info_icon-breeze.410d1c42.svg"
+  },
+  {
+    "revision": "c76c9ad5e78ae01d0d3fa0dafc4f71fc",
+    "url": "./static/media/info_icon-crawling-baby-silhouette.c76c9ad5.svg"
+  },
+  {
+    "revision": "6a06afa106f27c964bedd26ed0b5f0a0",
+    "url": "./static/media/info_icon-dog.6a06afa1.svg"
+  },
+  {
+    "revision": "ad2b689a8ee5572de3bcf622be325a7c",
+    "url": "./static/media/info_icon-mountain-range.ad2b689a.svg"
+  },
+  {
+    "revision": "74436c499fb4931b1afd28066d3e9b2d",
+    "url": "./static/media/info_icon-no-smoke-symbol.74436c49.svg"
+  },
+  {
+    "revision": "16543ec913c147480a05482652e096dc",
+    "url": "./static/media/info_icon-phone.16543ec9.svg"
+  },
+  {
+    "revision": "ef31df00d2233ac153e23339d52ce614",
+    "url": "./static/media/info_icon-room_service.ef31df00.svg"
+  },
+  {
+    "revision": "1874126e19440cf50976e2e4283685b1",
+    "url": "./static/media/info_icon-tick-inside-circle.1874126e.svg"
+  },
+  {
+    "revision": "7e9c444b1cb139df81d88466b495153b",
+    "url": "./static/media/info_icon-wifi.7e9c444b.svg"
+  },
+  {
+    "revision": "cc5e92b0767b99437ee32d191c9c2342",
+    "url": "./static/media/instagram-brands.cc5e92b0.svg"
+  },
+  {
+    "revision": "d4ac21609a8dcd4a84f625091b1cf5c1",
+    "url": "./static/media/logo_block.d4ac2160.svg"
+  },
+  {
+    "revision": "2540fc08d2e0e1afb6ccf1f049970d93",
+    "url": "./static/media/logo_white.2540fc08.svg"
+  },
+  {
+    "revision": "6dbf65e5c980bded8b6b6ca3b025af3f",
+    "url": "./static/media/phone-alt-solid.6dbf65e5.svg"
+  }
+]);
