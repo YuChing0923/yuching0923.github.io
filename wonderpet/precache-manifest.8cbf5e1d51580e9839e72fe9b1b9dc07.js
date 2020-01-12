e3f3ec45af566e4e2e982ef04ee8a725f9d64cf4
YuChing0923 <salia0923@gmail.com>
2020-01-12 20:34:50 +0800

    [react][wonderpet - bingo game][js/jsx/scss]

---
+++ wonderpet/precache-manifest.8cbf5e1d51580e9839e72fe9b1b9dc07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b21111d15476d0fc841d312d282037b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a811caec7700a16672f9",
+    "url": "./static/css/main.8b0064bb.chunk.css"
+  },
+  {
+    "revision": "b2f4b8d42ccd45bfed04",
+    "url": "./static/js/2.74c7c77a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.74c7c77a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a811caec7700a16672f9",
+    "url": "./static/js/main.5d58a6a1.chunk.js"
+  },
+  {
+    "revision": "40cc9400c77595da8c79",
+    "url": "./static/js/runtime-main.5f728877.js"
+  }
+]);
